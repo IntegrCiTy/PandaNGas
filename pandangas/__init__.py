f521d58601269d00071b53573fbcdcf29bbb2c2a
Pablo Puerto <pablo.puerto@crem.ch>
2018-03-20 08:05:26 +0100

    create setup

--- pandangas/__init__.py
+++ pandangas/__init__.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    This module allow to design and simulate natural gas network.
+"""
+
+__version__ = "0.0.1"
+
 from pandangas.core import Network
