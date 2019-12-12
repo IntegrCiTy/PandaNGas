71ad23c5c5c1b5a6026c9fc052da74b6d72a2304
Edmund Widl <edmund.widl@gmail.com>
2019-12-12 22:47:22 +0100

    Update __init__.py

--- pandangas/__init__.py
+++ pandangas/__init__.py
@@ -5,8 +5,6 @@
     This module allow to design and simulate natural gas network.
 """
 
-__version__ = "0.0.1"
-
 from pandangas.core import *
 from pandangas.results import *
 from pandangas.utilities import get_index
